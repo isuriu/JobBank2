cea82d8a734a3c47e1f53e0749167f87e7ffef94
isuriu <isuriudeshika322@gmail.com>
2021-12-31 00:08:46 +0530

    email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-
+<!--<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">-->
 <div class="flex-shrink-0 flex items-center">
     <a href="{{ route('dashboard') }}">
         <img src="{{asset('/img/logo3.png')}}" style="height:85px;">
