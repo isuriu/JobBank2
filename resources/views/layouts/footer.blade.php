55dbe71b7adaa5e81330f6b52b45064c1fbc247a
isuriu <isuriudeshika322@gmail.com>
2022-01-02 23:02:51 +0530

    welcome page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<footer>
+
+<div id="footer-wrap1">
+    <ul id="footer-menu">
+        <li><a href="/privacy.html" class="text"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">About privacy policy</font></font></span></a></li>
+        <li><a href="/senyou.html" class="text"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Employee-only page</font></font></span></a></li>
+    </ul>
+
+    <div id="footer-contents">
+        <table cellspacing="0" cellpadding="0" border="0">
+            <tbody>
+                <tr>
+                    <td width="52">&nbsp;</td>
+                    <td width="856" valign="top" align="center">
+                    <p><span style="font-size:120%; font-weight:bold"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Higashi Consultant Co., Ltd.</font></font></span><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> 
+                    TEL: 0550-88-1447 FAX: 0550-88-1450 890-10 Hagiwara, Gotemba City, Shizuoka Prefecture</font></font></p>
+                    </td>
+                    <td width="52" valign="bottom">
+                    <div id="page-top">
+                    <div id="toPageTop"><a href="#top" style="display: inline;"><img src="/asset/shared/layout/page-up.jpg" alt=""></a></div>
+                    </div>
+                    <!-- page-top --></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+        
+</div><!-- footer-wrap1 -->
+
+<div id="footer-wrap2">
+    <div id="footer-copyright"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+    Copyright (C) East Consultant All Rights Reserved.
+    </font></font></div><!-- footer-copyright -->
+
+</div><!-- footer-wrap2 -->
+
+
+
+</footer>
