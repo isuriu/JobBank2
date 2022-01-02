46ceea4cde241ec787c4608a183d2c2f864447be
isuriu <isuriudeshika322@gmail.com>
2022-01-02 23:24:39 +0530

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,8 @@
 
 <div id="footer-wrap1">
     <ul id="footer-menu">
-        <li><a href="/privacy.html" class="text"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">About privacy policy</font></font></span></a></li>
-        <li><a href="/senyou.html" class="text"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Employee-only page</font></font></span></a></li>
+        <li><a href="{{ url('/about') }}" class="text"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">About us</font></font></span></a></li>
+        <li><a href="{{ url('/find_job') }}" class="text"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Find Jobs</font></font></span></a></li>
     </ul>
 
     <div id="footer-contents">
@@ -12,7 +12,7 @@
                 <tr>
                     <td width="52">&nbsp;</td>
                     <td width="856" valign="top" align="center">
-                    <p><span style="font-size:120%; font-weight:bold"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Higashi Consultant Co., Ltd.</font></font></span><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> 
+                    <p><span style="font-size:120%; font-weight:bold"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">East Consultant Co., Ltd.</font></font></span><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> 
                     TEL: 0550-88-1447 FAX: 0550-88-1450 890-10 Hagiwara, Gotemba City, Shizuoka Prefecture</font></font></p>
                     </td>
                     <td width="52" valign="bottom">
@@ -30,7 +30,7 @@
 
 <div id="footer-wrap2">
     <div id="footer-copyright"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-    Copyright (C) East Consultant All Rights Reserved.
+    2021&nbsp;<span>&#169;</span> Azuma Consultant All Rights Reserved.
     </font></font></div><!-- footer-copyright -->
 
 </div><!-- footer-wrap2 -->
